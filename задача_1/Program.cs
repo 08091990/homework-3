26407e213dc0e0c15fd9367b82dcba1263cfcac2
Artem <Art-80919900@mail.ru>
2023-04-23 17:36:11 +0500

    дз

---
+++ задача_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    return (int.Parse(Console.ReadLine()));
+}
+int N = EnterData("Введите пятизначное число N: ");
+   if ((N /10) % 10 == (N / 1000) % 10)
+     if (N % 10 == N / 10000)
+  Console.WriteLine($"{N} -> да");
+  else
+  {
+  Console.WriteLine($"{N} -> нет");
+  }
---
+++ задача_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Расстояние = $ \ sqrt{(x2-x1) ^{2} + (y2-y1) ^{2} + (z2-z1) ^{2}}$ 
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    return (int.Parse(Console.ReadLine()));
+}
+int x1 = EnterData("Введите число x1: ");
+int y1 = EnterData("Введите число y1: ");
+int z1 = EnterData("Введите число z1: ");
+int x2 = EnterData("Введите число x2: ");
+int y2 = EnterData("Введите число y2: ");
+int z2 = EnterData("Введите число z2: ");
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"A ({x1}, {y1}, {z1}); B ({x2}, {y2}, {z2}); -> {distance}");
